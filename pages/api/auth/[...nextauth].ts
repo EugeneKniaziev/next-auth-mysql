0ce91ca64e36610fb86d06e7d83de17ae5af6535
Nik <cheliojul@gmail.com>
2022-02-03 15:23:32 +0200

    duplicate clean

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,7 +6,6 @@ import { PrismaClient } from '@prisma/client';
 // import EmailProvider from "next-auth/providers/email";
 import CredentialsProvider from 'next-auth/providers/credentials';
 // import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 const options = {
